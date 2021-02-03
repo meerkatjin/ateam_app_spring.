60450d25f083ac3db976e5b04d132135a5e1e185
302-15\hanul <meerkatjin@naver.com>
2021-02-03 17:37:46 +0900

    회원 VO한번 만들어봄 게터세터 안만듬 일부로

---
+++ ateam_web_spring/src/main/java/member/MemberVO.java
@@ -0,0 +1,15 @@
+package member;
+
+public class MemberVO {
+	private int user_id;
+	private String user_email;
+	private String user_pw;
+	private String user_nm;
+	private String user_addr;
+	private String user_pro_img;
+	private String user_phone_no;
+	private String user_grade;
+	private String user_type;
+	
+	
+}
