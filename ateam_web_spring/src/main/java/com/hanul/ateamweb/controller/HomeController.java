5e107413f7de03f01b9a55403cb3dd61506c60d4
302-06\hanul <vesteira2018@gmail.com>
2021-02-17 08:37:52 +0900

    풀받으려고

--- ateam_web_spring/src/main/java/com/hanul/ateamweb/controller/HomeController.java
+++ ateam_web_spring/src/main/java/com/hanul/ateamweb/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.hanul.ateamweb.controller;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@RequestMapping("/")
+	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		
 		return "main";
