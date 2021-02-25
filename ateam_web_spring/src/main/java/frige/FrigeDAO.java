27b33c52653ee28abdca582b8e68be33a2366670
302-06\hanul <vesteira2018@gmail.com>
2021-02-25 19:01:40 +0900

    냉장고 쪽 필터 구현중

--- ateam_web_spring/src/main/java/frige/FrigeDAO.java
+++ ateam_web_spring/src/main/java/frige/FrigeDAO.java
@@ -14,5 +14,59 @@ public class FrigeDAO implements FrigeService {
 	public List<FrigeVO> frige_list(long user_id) {
 		return sql.selectList("frige.mapper.frige_list", user_id);
 	}
+
+	@Override
+	public List<FrigeVO> frige_list_name(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_meat(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_fish(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_vege(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_fruit(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_dairy(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_grain(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_season(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_etc(long user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
--- ateam_web_spring/src/main/java/frige/FrigeServiceImpl.java
+++ ateam_web_spring/src/main/java/frige/FrigeServiceImpl.java
@@ -13,5 +13,50 @@ public class FrigeServiceImpl implements FrigeService {
 	public List<FrigeVO> frige_list(long user_id) {
 		return dao.frige_list(user_id);
 	}
+
+	@Override
+	public List<FrigeVO> frige_list_name(long user_id) {
+		return dao.frige_list_name(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_meat(long user_id) {
+		return dao.frige_list_meat(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_fish(long user_id) {
+		return dao.frige_list_fish(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_vege(long user_id) {
+		return dao.frige_list_vege(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_fruit(long user_id) {
+		return dao.frige_list_fruit(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_dairy(long user_id) {
+		return dao.frige_list_dairy(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_grain(long user_id) {
+		return dao.frige_list_grain(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_season(long user_id) {
+		return dao.frige_list_season(user_id);
+	}
+
+	@Override
+	public List<FrigeVO> frige_list_etc(long user_id) {
+		return dao.frige_list_etc(user_id);
+	}
 	
 }
