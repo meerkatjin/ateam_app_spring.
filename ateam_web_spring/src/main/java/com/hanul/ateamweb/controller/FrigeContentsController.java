f992e8dc91f8abeeecaa09526a7cf73ebc4ad4e1
302-06\hanul <vesteira2018@gmail.com>
2021-02-26 18:32:37 +0900

    ㅋㅁ

--- ateam_web_spring/src/main/java/com/hanul/ateamweb/controller/FrigeContentsController.java
+++ ateam_web_spring/src/main/java/com/hanul/ateamweb/controller/FrigeContentsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import common.CommonService;
 import frige.FrigeServiceImpl;
@@ -18,16 +19,21 @@ public class FrigeContentsController {
 	
 	//냉장고 페이지 요청
 	@RequestMapping("/view.fc")
-	public String frigeView(HttpSession session, Model model, String search, String keyword) {
+	public String frigeView(HttpSession session, Model model, String keyword, String filter, String type) {
 		session.setAttribute("category", "fc");
 		MemberVO member = (MemberVO)session.getAttribute("loginInfo");
 		if (member == null) {
 			return "member/login";
 		}
-		
 		long user_id = member.getUser_id();
 		
 		model.addAttribute("list", service.frige_list(user_id));
+		
 		return "frige/view";
+	}
+	
+	@ResponseBody @RequestMapping(value="/frige/date", produces="application/json; charset=utf-8")
+	public Object frige_date() {
+		return "";
 	}
 }
