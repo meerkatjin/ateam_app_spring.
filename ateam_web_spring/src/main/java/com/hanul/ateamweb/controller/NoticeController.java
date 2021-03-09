1f86cfcfe2f004fcf18ed7dff31b0de5d1f34b57
tpfk373@hanmail.net <tpfk373@hanmail.net>
2021-03-09 23:46:41 +0900

    공지사항만 검색기능 완료

--- ateam_web_spring/src/main/java/com/hanul/ateamweb/controller/NoticeController.java
+++ ateam_web_spring/src/main/java/com/hanul/ateamweb/controller/NoticeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import member.MemberVO;
 import common.BoardVO;
 import common.CommonService;
 import notice.NoticePage;
